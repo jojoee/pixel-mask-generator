5a7f6ed578c5759ea08efc0e7f58f26bb8ab1dfc
Nathachai Thongniran <inid3a@gmail.com>
2017-10-02 00:00:49 +0700

    test(util): count function

--- src/static/util.test.js
+++ src/static/util.test.js
@@ -5,3 +5,15 @@ describe('util', () => {
     expect(util).not.toBeInstanceOf(util)
   })
 })
+
+describe('count', () => {
+  test('general', () => {
+    const arr = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+    expect(util.count(arr, 0)).toBe(0)
+    expect(util.count(arr, 1)).toBe(1)
+    expect(util.count(arr, 2)).toBe(2)
+    expect(util.count(arr, 3)).toBe(3)
+    expect(util.count(arr, 4)).toBe(4)
+    expect(util.count(arr, 5)).toBe(0)
+  })
+})
